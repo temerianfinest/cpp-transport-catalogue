bd84d147586f6a25db49585af18700b77dfab65e
temerianfinest <126896272+temerianfinest@users.noreply.github.com>
2024-01-18 00:31:50 +0500

    Add files via upload

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -3,38 +3,37 @@
 
 namespace TransportSystem {
 
-StatReader::StatReader(std::istream& input_stream, std::ostream& output_stream)
-    : input_stream_(input_stream), output_stream_(output_stream) {}
-
-void StatReader::ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request) {
+void StatReader::ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request,
+                                   std::ostream& output) {
     std::string request_str = std::string(request);
 
     if (request_str.find("Bus") == 0) {
-        // Process bus request
+
         std::string bus_name = request_str.substr(4); 
 
         std::string result = transport_catalogue.GetBusRouteInfo(bus_name);
 
-        output_stream_ << result << std::endl;
+        output << result << std::endl;
     } else if (request_str.find("Stop") == 0) {
-        // Process stop request
+        
         std::string stop_name = request_str.substr(5); 
+
         const Stop* stop = transport_catalogue.GetStop(stop_name);
 
         if (stop) {
-            output_stream_ << "Stop " << stop_name << ":";
+            output << "Stop " << stop_name << ":";
             const auto& buses = transport_catalogue.GetBusesAtStop(stop_name);
             if (buses.empty()) {
-                output_stream_ << " no buses" << std::endl;
+                output << " no buses" << std::endl;
             } else {
-                output_stream_ << " buses";
+                output << " buses";
                 for (const auto& bus : buses) {
-                    output_stream_ << " " << bus;
+                    output << " " << bus;
                 }
-                output_stream_ << std::endl;
+                output << std::endl;
             }
         } else {
-            output_stream_ << "Stop " << stop_name << ": not found" << std::endl;
+            output << "Stop " << stop_name << ": not found" << std::endl;
         }
     }
 }
